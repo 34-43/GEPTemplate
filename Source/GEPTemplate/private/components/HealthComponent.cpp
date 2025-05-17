aced719f77ff84d1641771004754e60b2e73879a
34-43 <jehyuck3443@gmail.com>
2025-05-17 12:17:39 +0900

    Feat: 델리게이트 위주의 체력 컴포넌트 구현

---
+++ Source/GEPTemplate/private/components/HealthComponent.cpp
@@ -0,0 +1,27 @@
+﻿#include "components/HealthComponent.h"
+
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentHealth = MaxHealth;
+}
+
+void UHealthComponent::UpdateHealth(const int32 Delta)
+{
+	CurrentHealth = FMath::Clamp(CurrentHealth + Delta, 0, MaxHealth);
+
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+
+	if (CurrentHealth <= 0)
+	{
+		OnDeath.Broadcast();
+	}
+}
---
+++ Source/GEPTemplate/public/components/HealthComponent.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+	FOnHealthChangedSignature,
+	int32, NewHealth,
+	int32, MaxHealth
+);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GEPTEMPLATE_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHealthComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// 스테이터스
+	UPROPERTY(EditDefaultsOnly, Category = "Health") int32 MaxHealth = 100.0;
+	UPROPERTY(EditDefaultsOnly, Category = "Health") int32 CurrentHealth;
+
+	// 이벤트
+	UPROPERTY(BlueprintAssignable, Category = "Events") FOnHealthChangedSignature OnHealthChanged;
+	UPROPERTY(BlueprintAssignable, Category = "Events") FOnDeathSignature OnDeath;
+
+	UFUNCTION()
+	void UpdateHealth(int32 Delta);
+};
